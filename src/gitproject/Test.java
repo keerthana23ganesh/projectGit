f10142ad461ac35ddaedd859d7a20fe8d41c0872
keerthana23ganesh <keerthuece@gmail.com>
2024-06-05 15:31:30 +0530

    new code by keerthana

--- src/gitproject/Test.java
+++ src/gitproject/Test.java
@@ -4,4 +4,11 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("git project created");
 }
+private void addtokart() {
+	System.out.println("null");
+
+}
+private void payment() {
+	System.out.println("pointer");
+}
 }
