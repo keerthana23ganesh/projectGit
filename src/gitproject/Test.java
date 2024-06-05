d37da2b853fda759ebc425dc83b29c267f36436b
keerthana23ganesh <keerthuece@gmail.com>
2024-06-05 14:33:58 +0530

    new code by ganesh

--- src/gitproject/Test.java
+++ src/gitproject/Test.java
@@ -4,4 +4,8 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("git project created");
 }
+private void addtokart() {
+	System.out.println("i am working on add to kart");
+
+}
 }
